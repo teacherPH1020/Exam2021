05c29321a7684549d7cd471099a72b4e234c7d65
PH1020 teacher Sergey P <postnikov_s@teacher.itsrep.by>
2021-05-17 21:36:52 +0300

    templates
    Home templates

--- WeZdoms/settings.py
+++ WeZdoms/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'WeZdoms.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,6 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+import os
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
